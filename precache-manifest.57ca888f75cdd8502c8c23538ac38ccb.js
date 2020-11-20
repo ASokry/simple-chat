fe048f8db47f3b7e5b5cb5bf47a0a2014c9d98be
ASokry <anthonysokry11@gmail.com>
2020-11-19 16:54:21 -0800

    Updates

---
+++ precache-manifest.57ca888f75cdd8502c8c23538ac38ccb.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "0b7884e9684a0768cd90",
+    "url": "/simple-chat/static/css/main.a42ae555.chunk.css"
+  },
+  {
+    "revision": "0b7884e9684a0768cd90",
+    "url": "/simple-chat/static/js/main.c37ed8ae.chunk.js"
+  },
+  {
+    "revision": "3eccf0a71b8f5b74ffe4",
+    "url": "/simple-chat/static/js/runtime~main.86b44704.js"
+  },
+  {
+    "revision": "f4dce14c27d40e44a6bd",
+    "url": "/simple-chat/static/css/2.ab786e3e.chunk.css"
+  },
+  {
+    "revision": "f4dce14c27d40e44a6bd",
+    "url": "/simple-chat/static/js/2.b5f783d6.chunk.js"
+  },
+  {
+    "revision": "5ff34ead064a6cb27084ae53d207bf5c",
+    "url": "/simple-chat/index.html"
+  }
+];
